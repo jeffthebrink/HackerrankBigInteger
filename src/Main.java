6777d045f05ed8e587e1a8ab2b0ab12401c8a82c
jeffthebrink <jeffthebrink@gmail.com>
2017-09-26 17:10:48 -0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string1, string2;
+
+        string1 = scanner.nextLine();
+        string2 = scanner.nextLine();
+
+        System.out.println(new BigInteger(string1).add(new BigInteger(string2)));
+        System.out.println(new BigInteger(string1).multiply(new BigInteger(string2)));
+    }
+}
